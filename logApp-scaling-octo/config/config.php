7f977594b4d64cbc9e69ae2f0735b512151d7815
RalphDePaz <202080468@psu.palawan.edu.ph>
2022-10-12 05:06:09 +0800

    heroku commit

---
+++ logApp-scaling-octo/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6525781');
+	define('DB_PASS', 'XalKQ8mEDA');
+	define('DB_NAME', 'sql6525781');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ralphdepaz/logapp-de-paz',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bf3dca06955f59ec59f68918fd18f0cfec9dfdc5',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ralphdepaz/logapp-de-paz' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bf3dca06955f59ec59f68918fd18f0cfec9dfdc5',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
